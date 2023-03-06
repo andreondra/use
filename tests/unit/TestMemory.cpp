8d465f08e8c3abf83dec28e785799bd543d18937
Ondrej Golasowski <golasowski.o@gmail.com>
2023-03-06 10:25:57 +0100

    Added memory binary file load function.

--- tests/unit/TestMemory.cpp
+++ tests/unit/TestMemory.cpp
@@ -2,6 +2,7 @@
  * @file TestMemory.cpp Test Memory class.
  * */
 
+#include <fstream>
 #include "gtest/gtest.h"
 #include "components/Memory.h"
 
@@ -18,7 +19,7 @@ TEST(TestMemory, DefaultValues) {
         std::weak_ptr<Connector> mc = memory.getConnector("data");
         DataInterface di = mc.lock()->getDataInterface();
 
-        uint32_t buffer;
+        uint32_t buffer = 0x0;
         for(int i = 0x00; i < 0x10; i++) {
             EXPECT_TRUE(di.read(i, buffer));
             EXPECT_EQ(buffer, val);
@@ -39,7 +40,7 @@ TEST(TestMemory, DefaultValuesMirroring) {
         std::weak_ptr<Connector> mc = memory.getConnector("data");
         DataInterface di = mc.lock()->getDataInterface();
 
-        uint32_t buffer = 0;
+        uint32_t buffer = 0x0;
         for(int i = 0x0; i < 0x10; i++) {
             EXPECT_FALSE(di.read(i, buffer));
         }
@@ -65,7 +66,7 @@ TEST(TestMemory, ReadWrite) {
     std::weak_ptr<Connector> mc = memory.getConnector("data");
     DataInterface di = mc.lock()->getDataInterface();
 
-    uint32_t buffer;
+    uint32_t buffer = 0x0;
     for(int i = 0x00; i < 0x10; i++) {
 
         EXPECT_TRUE(di.read(i, buffer));
@@ -128,5 +129,71 @@ TEST(TestMemory, Init) {
     for(int i = 0x00; i < 0x10; i++) {
         EXPECT_TRUE(di.read(i, buffer));
         EXPECT_EQ(buffer, 0xFF);
+    }
+}
+
+TEST(TestMemory, Load) {
+
+    Memory memory(5, {0x00, 0x0F}, 0x00);
+
+    std::weak_ptr<Connector> mc = memory.getConnector("data");
+    DataInterface di = mc.lock()->getDataInterface();
+
+    std::ifstream fileSmall("testfiles/dummySmall.bin", std::ios_base::binary);
+    std::ifstream fileBig("testfiles/dummyBig.bin", std::ios_base::binary);
+    if(!fileSmall) throw std::runtime_error("Can't open file dummySmall.bin.");
+    if(!fileBig) throw std::runtime_error("Can't open file dummyBig.bin.");
+
+    // Try to specify invalid offsets.
+    for(uint32_t i = 0x6; i < 0xA; i++)
+        EXPECT_THROW(memory.load(i, fileSmall), std::invalid_argument);
+
+    memory.load(0x0, fileSmall);
+
+    uint32_t buffer = 0x0;
+    // Compare memory contents with the known contents of the dummySmall (sequence 0x1 - 0x5).
+    uint32_t expected = 0x1;
+    for(uint32_t i = 0x0; i < 0x5; i++) {
+        di.read(i, buffer);
+        EXPECT_EQ(buffer, expected++);
+    }
+
+    // Load bigger file, check.
+    memory.load(0x0, fileBig);
+    expected = 0xA;
+    for(uint32_t i = 0x0; i < 0x5; i++) {
+        di.read(i, buffer);
+        EXPECT_EQ(buffer, expected--);
+    }
+    // Test mirroring.
+    expected = 0xA;
+    for(uint32_t i = 0x5; i < 0xA; i++) {
+        di.read(i, buffer);
+        EXPECT_EQ(buffer, expected--);
+    }
+}
+
+TEST(TestMemory, LoadOffset) {
+
+    Memory memory(5, {0x00, 0x0F}, 0x00);
+
+    std::weak_ptr<Connector> mc = memory.getConnector("data");
+    DataInterface di = mc.lock()->getDataInterface();
+
+    std::ifstream fileSmall("testfiles/dummySmall.bin", std::ios_base::binary);
+
+    for(uint32_t offset = 0x0; offset < 0x5; offset++) {
+
+        memory.init();
+        memory.load(offset, fileSmall);
+        uint32_t expected = 0x0;
+        uint32_t buffer = 0x0;
+        for(uint32_t adr = 0x0; adr < 0x5; adr++) {
+            di.read(adr, buffer);
+            expected = (adr >= offset) ? expected + 1 : 0x0;
+            EXPECT_EQ(buffer, expected);
+        }
+
+        fileSmall.seekg(std::ios_base::beg);
     }
 }
