ee4cb1f11f4a6069987f7a36f2db8a84b0305b0c
Ondrej Golasowski <golasowski.o@gmail.com>
2023-03-08 09:55:34 +0100

    Added Trigger masking test.

--- tests/unit/TestTrigger.cpp
+++ tests/unit/TestTrigger.cpp
@@ -40,3 +40,32 @@ TEST(TestTrigger, Basic) {
     trigInterface.lock()->getDataInterface().write(0xABC, 0x1);
     EXPECT_TRUE(triggered);
 }
+
+TEST(TestTrigger, Masking) {
+
+    bool triggered = false;
+
+    Trigger t(0xABC, 0x80, 0x80);
+    std::shared_ptr<Connector> c = std::make_shared<Connector>(
+            SignalInterface{
+                    .send = [& triggered](){
+                        triggered = true;
+                    }
+            });
+
+    t.connect("target", c);
+    std::weak_ptr<Connector> trigInterface = t.getConnector("trigger");
+
+    EXPECT_FALSE(triggered);
+
+    trigInterface.lock()->getDataInterface().write(0xABC, 0x80);
+    EXPECT_TRUE(triggered);
+    triggered = false;
+
+    trigInterface.lock()->getDataInterface().write(0xABC, 0xFF);
+    EXPECT_TRUE(triggered);
+    triggered = false;
+
+    trigInterface.lock()->getDataInterface().write(0xABC, 0x7F);
+    EXPECT_FALSE(triggered);
+}
