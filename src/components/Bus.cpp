96d68cc489e7ad1cebf8fac116dd0ad25b64a116
Ondrej Golasowski <golasowski.o@gmail.com>
2023-03-19 10:56:35 +0100

    Added GUI to bus

--- src/components/Bus.cpp
+++ src/components/Bus.cpp
@@ -2,6 +2,7 @@
 // Created by golas on 23.2.23.
 //
 
+#include "imgui.h"
 #include "components/Bus.h"
 
 Bus::Bus(int portCount, int addrWidth, int dataWidth) : m_devices(portCount) {
@@ -16,6 +17,8 @@ Bus::Bus(int portCount, int addrWidth, int dataWidth) : m_devices(portCount) {
 
     m_dataMask = (~(uint32_t)0x0) >> (32 - dataWidth);
 
+    m_deviceName = "Bus";
+
     Connector masterConnector(
         DataInterface{
             .read = [&](uint32_t address, uint32_t & buffer) {
@@ -23,6 +26,10 @@ Bus::Bus(int portCount, int addrWidth, int dataWidth) : m_devices(portCount) {
                 for(auto & device : m_devices)
                     if(device.readConfirmed(address & m_addrMask, buffer)) {
                         buffer &= m_dataMask;
+
+                        m_lastAccess = lastAccess::READ;
+                        m_lastAddress = address & m_addrMask;
+                        m_lastData = buffer;
                         return true;
                     }
 
@@ -32,6 +39,10 @@ Bus::Bus(int portCount, int addrWidth, int dataWidth) : m_devices(portCount) {
 
                 for(auto & device : m_devices)
                     device.write(address & m_addrMask, data & m_dataMask);
+
+                m_lastAccess = lastAccess::WRITE;
+                m_lastAddress = address & m_addrMask;
+                m_lastData = data & m_dataMask;
             }
         }
     );
@@ -49,5 +60,40 @@ void Bus::init() {
 
 std::vector<EmulatorWindow> Bus::getGUIs() {
 
-    return {};
+    std::function<void(void)> debugger = [this](){
+
+        // Window contents
+        // ===================================================================
+        ImGui::Text("Address mask: 0x%x", m_addrMask);
+        ImGui::Text("Data mask: 0x%x", m_dataMask);
+        ImGui::Text("Connected devices: %lu", m_devices.size());
+        ImGui::Separator();
+        ImGui::Text("Last access");
+        if(m_lastAccess != lastAccess::NONE) {
+
+            ImGui::Text("Type: ");
+            ImGui::SameLine();
+            switch (m_lastAccess) {
+                case lastAccess::READ: ImGui::Text("read"); break;
+                case lastAccess::WRITE: ImGui::Text("write"); break;
+                default: ImGui::Text("unknown"); break;
+            }
+
+            ImGui::Text("At address: 0x%x", m_lastAddress);
+            ImGui::Text("Data: 0x%x", m_lastData);
+        } else {
+            ImGui::Text("There was no operation on the bus.");
+        }
+
+    };
+
+    return {
+            EmulatorWindow{
+                    .category = m_deviceName,
+                    .title = "Debugger",
+                    .id    = getDeviceID(),
+                    .dock  = DockSpace::LEFT,
+                    .guiFunction = debugger
+            }
+    };
 }
