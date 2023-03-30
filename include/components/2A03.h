a6329975fb05bc0b740a0ba17ace43b90f19f7fd
Ondrej Golasowski <golasowski.o@gmail.com>
2023-03-30 10:31:38 +0200

    Created 2A03 from 6502

---
+++ include/components/2A03.h
@@ -0,0 +1,18 @@
+//
+// Created by golas on 30.3.23.
+//
+
+#ifndef USE_2A03_H
+#define USE_2A03_H
+
+#include "components/6502.h"
+
+class RP2A03 : public MOS6502 {
+
+protected:
+
+public:
+    void init() override;
+};
+
+#endif //USE_2A03_H
