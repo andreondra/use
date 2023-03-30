e6360dde58e91662898438c795883cc3556bdc9c
Ondrej Golasowski <golasowski.o@gmail.com>
2023-03-30 09:07:52 +0200

    Added Tools: bitmap renderer, map

---
+++ include/Tools.h
@@ -0,0 +1,29 @@
+/**
+ * @file Tools.h
+ * @author Ondrej Golasowski (golasowski.o@gmail.com)
+ * @brief Helper functions.
+ * @copyright Copyright (c) 2023 Ondrej Golasowski
+ *
+ */
+
+#ifndef USE_TOOLS_H
+#define USE_TOOLS_H
+
+#include "imgui.h"
+#include "Types.h"
+
+namespace USETools {
+
+    /**
+     * Render a bitmap.
+     *
+     * @param pixelData Bitmap data in a form of: vector of rows, which are vectors of pixels (rows->row->pixel) => pixelData[y][x].
+     * @param scale Scale of the bitmap.
+     *
+     * @note Rows can be non-homogenous (having every row of a different size is allowed).
+     * @throw std::invalid_argument When pixel data is empty.
+     * */
+    void renderScalableBitmap(const std::vector<std::vector<RGBPixel>> & pixelData, float scale = 1.0f);
+}
+
+#endif //USE_TOOLS_H
