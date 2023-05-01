879e04b546f0cca5c0157d9e22fc0ca316f408f5
Ondrej Golasowski <golasowski.o@gmail.com>
2023-05-01 23:27:57 +0200

    Added docs to Emulator.h.

--- include/Emulator.h
+++ include/Emulator.h
@@ -1,6 +1,10 @@
-//
-// Created by golas on 21.2.23.
-//
+/**
+ * @file Emulator.h
+ * @author Ondrej Golasowski (golasowski.o@gmail.com)
+ * @brief Main platform class.
+ * @copyright Copyright (c) 2023 Ondrej Golasowski
+ *
+ */
 
 #ifndef USE_EMULATOR_H
 #define USE_EMULATOR_H
@@ -11,23 +15,37 @@
 #include "System.h"
 #include "ImInputBinder.h"
 
+/**
+ * Main platform class.
+ *
+ * This class represents the whole application. It handles GUI rendering (both global and System's), System loading,
+ * audio data interchange between System and sound management class (Sound) and input actions handling
+ * using ImInputBinder.
+ *
+ * As a developer of a new System, you usually want to add your new system here. As a Component developer,
+ * you should not change anything here.
+ * */
 class Emulator{
-
 private:
-    unsigned long long m_clockCounter = 0;
-    static const int DEFAULT_IMGUI_REFRESH_HZ = 60;
+    // ===========================================
+    // States
+    // ===========================================
+    enum class SYSTEMS { NONE, BARE6502, NES } m_systemID = SYSTEMS::NONE;
+    enum class STATE { STOPPED, RUNNING } m_runState = STATE::STOPPED;
 
-    ImInputBinder m_inputs;
-    bool m_showBindingsWindow = false;
-
-    enum class SYSTEMS{ NONE, BARE6502, NES } m_systemID = SYSTEMS::NONE;
+    // ===========================================
+    // Widgets, plug-ins and other components
+    // ===========================================
     std::unique_ptr<System> m_system;
     std::unique_ptr<Sound> m_sound;
-    enum class STATE{ STOPPED, RUNNING } m_runState = STATE::STOPPED;
+    ImInputBinder m_inputs;
+    bool m_showBindingsWindow = false;
 
     // ===========================================
     // Helpers
     // ===========================================
+    /// Used to keep track of elapsed clocks to sync emulation with sound output.
+    unsigned long long m_clockCounter = 0;
     /**
      * This function maps DockSpace enum to string, which is used by ImGui docking feature.
      *
@@ -53,6 +71,10 @@ private:
      * @param system A system to load.
      * */
     void loadSystem(std::unique_ptr<System> system);
+
+    /**
+     * Emulate the System if enabled.
+     * */
     void runSystem();
 
     // ===========================================
@@ -65,6 +87,13 @@ private:
 
 public:
     Emulator() = default;
+
+    /**
+     * @brief Run the application.
+     *
+     * This is the main application loop, return only on exit or error.
+     * @return 0 on success, other values on error.
+     * */
     int run();
 };
 
