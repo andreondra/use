866c87d12d517b926728002e8cb6590a4c1204fa
Ondrej Golasowski <golasowski.o@gmail.com>
2023-05-01 23:27:40 +0200

    Added docs to System.h and final specified to getName in System.h.

--- include/System.h
+++ include/System.h
@@ -30,28 +30,48 @@ public:
     System();
     virtual ~System() = default;
 
+    /**
+     * Initialize a System to a power-on state. Default implementation just calls init on all components in m_components.
+     *
+     * @note For System developer: If there is any other operation except initializing all the components,
+     * you can override the method and implement the initialization yourself.
+     * */
     virtual void init();
 
     /**
      * Proceed specified number of system clocks.
+     *
+     * @note For System developer: implement a reaction to a main clock signal. Normally whole emulation
+     * is driven by this function.
+     *
      * @param count Number of clock to process.
      * */
     virtual void doClocks(unsigned int count)           = 0;
 
     /**
      * Proceed specified number of CPU instructions.
+     *
+     * @note For System developer: You can for example proceed by single clocks until the CPU processes
+     * specified number of instructions. If there is no CPU, provide dummy method.
+     *
      * @param count Number of instructions to process.
      * */
     virtual void doSteps(unsigned  int count)           = 0;
 
     /**
      * Proceed specified number of rendered frames.
+     *
+     * @note For System developer: If there is neither GPU or any definition of frame, provide dummy method.
+     *
      * @param count Number of frames to process.
      * */
     virtual void doFrames(unsigned int count)           = 0;
 
     /**
      * Run system in real-time.
+     *
+     * @note For System developer:
+     *
      * @param updateFrequency Frequency in which the function is called to match the clock speed.
      * */
     virtual void doRun(unsigned int updateFrequency)    = 0;
@@ -97,7 +117,7 @@ public:
     virtual std::vector<ImInputBinder::action_t> getInputs();
 
     /// Get system's name.
-    [[nodiscard]] virtual std::string getName() const;
+    [[nodiscard]] virtual std::string getName() const final;
 };
 
 #endif //USE_SYSTEM_H
